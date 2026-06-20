76c0cf3047528611a5b82d51dd97a8186afd2aca
Luan Gustavo <lgns@cin.ufpe.br>
2026-06-20 09:03:08 -0300

    front: remove rotas existentes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,49 +1,23 @@
-import { createRouter, createWebHistory, NavigationGuardNext } from 'vue-router';
+import { createRouter, createWebHistory, NavigationGuardNext, RouteRecordRaw } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
-import Home from '../views/Home.vue';
-import Login from '../views/Login.vue';
-import Admin from '../views/Admin.vue';
 
-import Exemplos from '../views/Exemplos.vue';
-import Pacientes from '../views/Pacientes.vue';
-
-const routes = [
+const routes: RouteRecordRaw[] = [
+  /*
+  Exemplo:
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-    meta: { layout: 'LoginLayout' },
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: Admin,
-    meta: { requiresAuth: true },
-  },
-
-  {
-    path: '/exemplos',
-    name: 'Exemplos',
-    component: Exemplos,
-  },
-  {
-    path: '/pacientes',
-    name: 'Pacientes',
-    component: Pacientes,
-    meta: { requiresAuth: true },
-  },
+    path: "/caminho",
+    name: "nome da rota",
+    component: Componente,
+    meta: {
+      layout: ComponenteDeLayout
+    }
+  }
+  */
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-  linkActiveClass: 'bg-paper-active-link',
-  linkExactActiveClass: 'bg-paper-active-link',
+  routes
 });
 
 router.beforeEach((to, _from, next: NavigationGuardNext) => {
