d275c60351b7008e6f9b9698156e88343079abab
lecs2 <cavalcantesantosluiseduardo@gmail.com>
2026-06-25 15:12:40 -0300

    chore: remove importação de módulo removido

--- src/main.py
+++ src/main.py
@@ -68,8 +68,7 @@ else:
     print("Aviso: Diretórios de build do frontend não encontrados. (Normal em ambiente de desenvolvimento)")
 
 # Placeholder para incluir os roteadores da API
-from .routers import paciente, auth, admin, aih, bpa, material, forms, funcionario
-app.include_router(paciente.router)
+from .routers import auth, admin, aih, bpa, material, forms, funcionario
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(aih.router)
